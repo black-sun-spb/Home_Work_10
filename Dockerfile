3ddd5449940731acc2eb4a3f4943384db3c562fd
Taisiya Gorodnicheva <taisiya-1992@nail.ru>
2025-12-06 18:27:37 +0300

    исправление файлов

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
+RUN pip install gunicorn
 
 COPY . .
--- home_work_7/urls.py
+++ home_work_7/urls.py
@@ -1,10 +1,12 @@
-# home_work_7/urls.py
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.http import JsonResponse
+from django.utils import timezone
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,18 +19,22 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 
+def health_check(request):
+    return JsonResponse({
+        "status": "healthy",
+        "timestamp": timezone.now().isoformat(),
+        "service": "LMS API"
+    })
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # namespace 'api' нужен для reverse('api:lesson-list') и course-detail/subscription
     path('api/', include(('lms.urls', 'lms'), namespace='api')),
     path('api/', include(('users.urls', 'users'), namespace='users')),
-
-    # JWT токены
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('lms.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('health/', health_check),
 ]
