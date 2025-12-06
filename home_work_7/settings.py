1e2badb24651f0889bc32ea04f036776ca0763db
Taisiya Gorodnicheva <taisiya-1992@nail.ru>
2025-12-06 20:06:45 +0300

    исправление файлов

--- home_work_7/settings.py
+++ home_work_7/settings.py
@@ -116,8 +116,15 @@ USE_TZ = True
 # ====================================
 # Static files
 # ====================================
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # ====================================
 # Auth
